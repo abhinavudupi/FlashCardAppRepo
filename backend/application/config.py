7eacfaadbc95dc3308aac157bee4507c0b47c56c
Abhinav Udupi <info.pilabs@gmail.com>
2024-08-26 19:43:18 +0530

    edited db config

--- backend/application/config.py
+++ backend/application/config.py
@@ -3,7 +3,6 @@ import os
 baseDir = os.path.abspath(os.path.dirname(__file__))
 class Config():
     DEBUG = False
-    SQLITE_DB_DIR = None
     SQLALCHEMY_DATABASE_URI = None
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CELERY_BROKER_URL = "redis://10.1.1.43:6379/1"
@@ -15,10 +14,13 @@ class Config():
     CACHE_REDIS_PORT=6379
 
 class LocalDevConfig(Config):
-    SQLITE_DB_DIR = os.path.join(baseDir, "../data")
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(SQLITE_DB_DIR, 'testdb.sqlite3')
+    user = 'abhinav'
+    password = 'abhinavsql'
+    host = 'localhost'
+    port = 5432
+    database = 'devdb'
+    SQLALCHEMY_DATABASE_URI = ('postgresql://{}:{}@{}:{}/{}'.format(user, password, host, port, database))
     DEBUG = True
-    REDIS_URL = "redis://10.1.1.43:6379"
 
 class ProductionConfig(Config):
     user = 'flashcardApp'
@@ -27,4 +29,3 @@ class ProductionConfig(Config):
     port = 5432
     database = 'flashCardDB'
     SQLALCHEMY_DATABASE_URI = ('postgresql://{}:{}@{}:{}/{}'.format(user, password, host, port, database))
-    DEBUG = False
